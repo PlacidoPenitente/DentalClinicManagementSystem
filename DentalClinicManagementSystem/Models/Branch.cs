66f5113b8724c9f5e448d34acdbb405b29ec36e3
Jay Mark Estrera <estrerajaymark@ymail.com>
2018-08-01 11:19:23 +0800

    Finished Branch CRUD.

--- DentalClinicManagementSystem/Models/Branch.cs
+++ DentalClinicManagementSystem/Models/Branch.cs
@@ -7,14 +7,14 @@
     public sealed class Branch : Model, IBranch
     {
         [DisplayName("Name")]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Branch Name is required.")]
-        [MaxLength(255, ErrorMessage = "Branch Name is too long.")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Name is required.")]
+        [MaxLength(255, ErrorMessage = "Name is too long.")]
         [Index(IsUnique = true)]
         public string Name { get; set; }
 
         [DisplayName("Address")]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Branch Address is required.")]
-        [MaxLength(255, ErrorMessage = "Branch Address is too long.")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Address is required.")]
+        [MaxLength(255, ErrorMessage = "Address is too long.")]
         public string Address { get; set; }
     }
 }
