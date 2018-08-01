4ee5f7e8cbbddfdbd68c3179878ff33492d2eb48
Jay Mark Estrera <estrerajaymark@ymail.com>
2018-08-01 10:44:14 +0800

    Restored Homepage.

--- DentalClinicManagementSystem/Models/ApplicationDbContext.cs
+++ DentalClinicManagementSystem/Models/ApplicationDbContext.cs
@@ -4,10 +4,17 @@
 
     public class ApplicationDbContext : DbContext
     {
-
-        public ApplicationDbContext() : base("dcmsdb")
+        public ApplicationDbContext()
+            : base("dcmsdb")
         {
-            
         }
+        
+        public DbSet<Branch> Branches { get; }
+
+        public DbSet<SecurityQuestion> SecurityQuestions { get; set; }
+
+        public DbSet<Staff> Staves { get; set; }
+
+        public DbSet<Dentist> Dentists { get; set; }
     }
 }
